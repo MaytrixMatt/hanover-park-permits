a2d4c2103ff132a920991921ba7edf520ab588e2
Matt Collins <mcollins24@hanoverstudents.org>
2023-03-20 12:11:54 -0400

    db push

---
+++ db/create-statement.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `fields` (
+  `fld_id` int NOT NULL AUTO_INCREMENT,
+  `fld_loc_id` int NOT NULL,
+  `fld_name` varchar(45) NOT NULL,
+  `fld_reserved` binary(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`fld_id`),
+  UNIQUE KEY `fld_id_UNIQUE` (`fld_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `fieldpermits`.`customers` (
+  `cus_id` INT NOT NULL AUTO_INCREMENT,
+  `cus_first_name` VARCHAR(45) NOT NULL,
+  `cus_last_name` VARCHAR(45) NOT NULL,
+  `cus_organization_name` VARCHAR(100) NOT NULL,
+  `cus_address` VARCHAR(100) NOT NULL,
+  `cus_phone` VARCHAR(45) NOT NULL,
+  `cus_email` VARCHAR(100) NOT NULL,
+  `cus_tier` INT NOT NULL,
+  PRIMARY KEY (`cus_id`),
+  UNIQUE INDEX `cus_id_UNIQUE` (`cus_id` ASC) VISIBLE);
+CREATE TABLE `location` (
+  `loc_id` int NOT NULL AUTO_INCREMENT,
+  `loc_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`loc_id`),
+  UNIQUE KEY `cus_id_UNIQUE` (`loc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `applications` (
+  `app_id` int NOT NULL AUTO_INCREMENT,
+  `app_cus_first_name` varchar(45) DEFAULT NULL,
+  `app_cus_last_name` varchar(45) DEFAULT NULL,
+  `app_tier` INT NOT NULL,
+  `app_afl_id` varchar(45) DEFAULT NULL,
+  `app_date_req` VARCHAR(45) NOT NULL,
+  `app_start_time` VARCHAR(45) NOT NULL,
+  `app_end_time` VARCHAR(45) NOT NULL,
+  `app_desc` VARCHAR(200) NULL,
+  `app_estim_people` INT NOT NULL,
+  PRIMARY KEY (`app_id`),
+  UNIQUE KEY `app_id_UNIQUE` (`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
