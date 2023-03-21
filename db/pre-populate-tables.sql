9d231350e2dfe9261a381f32018193565f001171
Matt Collins <mcollins24@hanoverstudents.org>
2023-03-21 12:40:37 -0400

    morechanges

--- db/pre-populate-tables.sql
+++ db/pre-populate-tables.sql
@@ -1,6 +1,18 @@
+-- FACILITY TABLE
 insert into location(loc_name) value('Briggs Field');
 insert into location(loc_name) value('Ceurvels Field');
 insert into location(loc_name) value('Calvin J. Ellis Field');
 insert into location(loc_name) value('Forge Pond Park');
 insert into location(loc_name) value('Amos Gallant Field');
 insert into location(loc_name) value('B. Everett Hall');
+
+-- FIELDS TABLE
+insert into fields(fld_loc_id, fld_name) value(1, 'T-Ball');
+insert into fields(fld_loc_id, fld_name) value(1, 'Other');
+insert into fields(fld_loc_id, fld_name) value(2, 'Full-size Baseball');
+insert into fields(fld_loc_id, fld_name) value(2, 'Little League');
+insert into fields(fld_loc_id, fld_name) value(2, 'Softball');
+insert into fields(fld_loc_id, fld_name) value(2, 'Lacrosse');
+insert into fields(fld_loc_id, fld_name) value(2, 'Soccer');
+insert into fields(fld_loc_id, fld_name) value(2, 'Basketball');
+insert into fields(fld_loc_id, fld_name) value(2, 'Other');
