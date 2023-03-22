442bb85791579e9b5a1036a2b892136b12fb7c2e
Daniel Nguyen <dnguyen23@hanoverstudents.org>
2023-03-22 12:17:04 -0400

    jquery

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 //This function works on displaying the fields based on whichever facility was selected
 // This should modify html code ***
-function displayFields()
+function displayFields() {
+    // 1.) check the option that was chosen
+    
+}
--- web/permitForm.php
+++ web/permitForm.php
@@ -2,6 +2,8 @@
 <html>
     <head> 
         <title>Field Park Permits</title>
+        <script src="scripts.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
 
     <body> 
