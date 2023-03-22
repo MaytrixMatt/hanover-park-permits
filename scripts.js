281cca5da3ec03483d4c1c61803a90318fe71ebf
Daniel Nguyen <dnguyen23@hanoverstudents.org>
2023-03-22 12:33:18 -0400

    js field function appending process

--- scripts.js
+++ scripts.js
@@ -5,8 +5,41 @@ function displayFields() {
     var facilityID = "#facility";
     var facility = $(facilityID).val();
 
+    var fieldElem = $("#field_requested");
     // 2.) check the facility and decide what fields to display
-    if (facility == ) {
-        
+    if (facility == "f1") {
+        fieldElem.append(
+            
+        );
+    } else if (facility == "f2") {
+        fieldElem.append(
+
+        );
+    } else if (facility == "f3") {
+        fieldElem.append(
+
+        );
+    } else if (facility == "f4") {
+        fieldElem.append(
+
+        );
+    } else if (facility == "f5") {
+        fieldElem.append(
+
+        );
+    } else if (facility == "f6") {
+        fieldElem.append(
+
+        );
     }
+
+    var mySelect = $('#mySelect');
+
+    mySelect.append(
+        $('<option></option>').val(val).html(text)
+    );
+
+
+
+
 }
