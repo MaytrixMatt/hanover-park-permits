b45323693680e52c6adb63c0f2b9437797a703e7
Matt Collins <52825996+MaytrixMatt@users.noreply.github.com>
2023-03-24 15:52:55 -0400

    Update scripts.js

--- web/scripts.js
+++ web/scripts.js
@@ -1,8 +1,6 @@
-
-
-
 //This function works on displaying the fields based on whichever facility was selected
 // This should modify html code ***
+
 function displayFields() {
 
     // Setup: run query to database to get all facilites with their fields
@@ -12,17 +10,12 @@ function displayFields() {
     // Then based on what facility was chosen, display the fields in a drop down
     
 
-
     // 1.) check the option that was chosen
     var mySelect = $('#mySelect');
 
     mySelect.append(
         $('<option></option>').val(val).html(text)
     );
-
-
-// Comment
-
 }
 
-// var fieldNameGrid = $()
+// var fieldNameGrid = $();
