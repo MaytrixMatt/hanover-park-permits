86263b1226825be9d798144152529809fe5a54a5
Matt Collins <mcollins24@hanoverstudents.org>
2023-04-03 12:16:28 -0400

    deleted stuff already in permitFrom

--- web/timeCheck.php
+++ web/timeCheck.php
@@ -8,15 +8,6 @@
         if(!isset($reqDate)) {
             $reqDate = "2023-01-01"; 
         }
-        // if(!isset($month_id)){
-        //     $month_id = 1;
-        // }
-        // if(!isset($day_id)){
-        //     $day_id = 1;
-        // }
-        // if(!isset($year_id)){
-        //     $year_id = 1;
-        // }
     ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <meta charset="UTF-8">
@@ -321,72 +312,6 @@
 
         <input type='date' id='reqDate'>
         <button id='date_button' onclick='checkDate()'>Check this date</button>
-        <!-- <div id = "monthList">
-            <label for="monthlist" class="field-form-label">Month</label>
-            <select id="monthlist" name="monthlist" onchange="updateMonth()">
-                <option value="1"<?php if($month_id==1)echo "Selected"; ?>>January</option>
-                <option value="2"<?php if($month_id==2)echo "Selected"; ?>>February</option>
-                <option value="3"<?php if($month_id==3)echo "Selected"; ?>>March</option>
-                <option value="4"<?php if($month_id==4)echo "Selected"; ?>>April</option>
-                <option value="5"<?php if($month_id==5)echo "Selected"; ?>>May</option>
-                <option value="6"<?php if($month_id==6)echo "Selected"; ?>>June</option>
-                <option value="7"<?php if($month_id==7)echo "Selected"; ?>>July</option>
-                <option value="8"<?php if($month_id==8)echo "Selected"; ?>>August</option>
-                <option value="9"<?php if($month_id==9)echo "Selected"; ?>>September</option>
-                <option value="10"<?php if($month_id==10)echo "Selected"; ?>>October</option>
-                <option value="11"<?php if($month_id==11)echo "Selected"; ?>>November</option>
-                <option value="12"<?php if($month_id==12)echo "Selected"; ?>>December</option>
-            </select>
-        
-
-
-
-            <label for="priority" class="day-form-label">Day</label>
-            <select id="day" name="priority" onchange = "updateDays()">
-                <option value="1"<?php if($day_id==1)echo "Selected"; ?>>1</option>
-                <option value="2"<?php if($day_id==2)echo "Selected"; ?>>2</option>
-                <option value="3"<?php if($day_id==3)echo "Selected"; ?>>3</option>
-                <option value="4"<?php if($day_id==4)echo "Selected"; ?>>4</option>
-                <option value="5"<?php if($day_id==5)echo "Selected"; ?>>5</option>
-                <option value="6"<?php if($day_id==6)echo "Selected"; ?>>6</option>
-                <option value="7"<?php if($day_id==7)echo "Selected"; ?>>7</option>
-                <option value="8"<?php if($day_id==8)echo "Selected"; ?>>8</option>
-                <option value="9"<?php if($day_id==9)echo "Selected"; ?>>9</option>
-                <option value="10"<?php if($day_id==10)echo "Selected"; ?>>10</option>
-                <option value="11"<?php if($day_id==11)echo "Selected"; ?>>11</option>
-                <option value="12"<?php if($day_id==12)echo "Selected"; ?>>12</option>
-                <option value="13"<?php if($day_id==13)echo "Selected"; ?>>13</option>
-                <option value="14"<?php if($day_id==14)echo "Selected"; ?>>14</option>
-                <option value="15"<?php if($day_id==15)echo "Selected"; ?>>15</option>
-                <option value="16"<?php if($day_id==16)echo "Selected"; ?>>16</option>
-                <option value="17"<?php if($day_id==17)echo "Selected"; ?>>17</option>
-                <option value="18"<?php if($day_id==18)echo "Selected"; ?>>18</option>
-                <option value="19"<?php if($day_id==19)echo "Selected"; ?>>19</option>
-                <option value="20"<?php if($day_id==20)echo "Selected"; ?>>20</option>
-                <option value="21"<?php if($day_id==21)echo "Selected"; ?>>21</option>
-                <option value="22"<?php if($day_id==22)echo "Selected"; ?>>22</option>
-                <option value="23"<?php if($day_id==23)echo "Selected"; ?>>23</option>
-                <option value="24"<?php if($day_id==24)echo "Selected"; ?>>24</option>
-                <option value="25"<?php if($day_id==25)echo "Selected"; ?>>25</option>
-                <option value="26"<?php if($day_id==26)echo "Selected"; ?>>26</option>
-                <option value="27"<?php if($day_id==27)echo "Selected"; ?>>27</option>
-                <option value="28"<?php if($day_id==28)echo "Selected"; ?>>28</option>
-                <option value="29"<?php if($day_id==29)echo "Selected"; ?>>29</option>
-                <option value="30"<?php if($day_id==30)echo "Selected"; ?>>30</option>
-                <option value="31"<?php if($day_id==31)echo "Selected"; ?>>31</option>
-            </select>
-            <label for="yearlist" class="year-form-label">Year</label>
-            <select id="yearlist" name="yearlist" onchange="updateYear()">
-                <option value="1"<?php if($year_id==1)echo "Selected"; ?>>2023</option>
-                <option value="2"<?php if($year_id==2)echo "Selected"; ?>>2024</option>
-            </select>
-        </div> -->
-        <!-- <div id = "fieldList">
-            <label for="priority" class="field-form-label">Field</label>
-            <select id="field" name="priority" onchange="">
-            </select>
-        </div> -->
-        <br>
         <br>
         <br>
         Fields
